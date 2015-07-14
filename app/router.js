ef28c2080b9b9bf25e5dd4e9c905a63dbe5140b1
WitmerDesigns <patrick@witmerdesigns.com>
2015-07-14 13:48:59 -0600

    July 14 progress
    current progress update, 7.14.

--- app/router.js
+++ app/router.js
@@ -6,9 +6,12 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.resource('friends', function(){  });
-  this.route('friends', function() {
+  this.resource('friends', function(){  
     this.route('new');
+
+    this.route('show', {
+      path: ':friend_id'
+    });
   });
 });
 
