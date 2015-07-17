0d6a4d3fb97bfd6629ef03f516df6535510da177
WitmerDesigns <patrick@witmerdesigns.com>
2015-07-17 12:52:27 -0600

    stable commit

--- app/router.js
+++ app/router.js
@@ -10,14 +10,14 @@ Router.map(function() {
 
     this.route('new');
 
-    this.route('show', {      path: ':friend_id'     });
+    this.route('show', {      path: ':friend_id'     }, function() {
+      this.resource('articles', function() {  });
+    });
 
-    this.route('edit', {      path: ':friend_id/edit'});
-    
+    this.route('edit', {      path: ':friend_id/edit' });
+
   });
 
-
-  this.route('articles');
 });
 
 export default Router;
